6abb88be33679b1b73bc27d0b395834eb738f82b
joslo2345 <soulsilversx@gmail.com>
2022-04-30 16:16:01 -0500

    3: agreagando mas codigo a app.js

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ require('dotenv').config()
 if(!process.env.TOKEN && !process.env.KEY){
   throw new Error('No hay configuración con Api Key y con Token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "626d7d16c0350f436b8ce1ac",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	});
